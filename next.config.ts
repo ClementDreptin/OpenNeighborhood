c8f27f73ccb24fbfa9dda208a6d9d633102247db
Clément Dreptin <cdreptin@gmail.com>
2025-01-13 22:22:27 +0100

    ci: first try at a github workflow

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
