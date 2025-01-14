5437fe573e9a616bcbf1ced2f4c022dd60e29dd3
Clément Dreptin <cdreptin@gmail.com>
2025-01-14 22:25:38 +0100

    fix: set icon priority to true by default to fix LCP problem

--- src/components/ui/icon-button.tsx
+++ src/components/ui/icon-button.tsx
@@ -3,27 +3,31 @@ import Image from "next/image";
 import { Button, type ButtonProps } from "./button";
 import { cn } from "@/lib/utils";
 
-const IconButton = React.forwardRef<
-  HTMLButtonElement,
-  ButtonProps & { iconSrc: React.ComponentProps<typeof Image>["src"] }
->(({ className, iconSrc, children, ...props }, ref) => (
-  <Button
-    variant="outline"
-    className={cn("h-full w-full justify-around gap-4 text-wrap", className)}
-    ref={ref}
-    {...props}
-  >
-    <Image alt="" src={iconSrc} />
-    <div className="flex-grow self-start">
-      <div
-        style={{ overflowWrap: "anywhere" }}
-        className="line-clamp-3 text-start"
-      >
-        {children}
+interface IconButtonProps extends ButtonProps {
+  iconSrc: React.ComponentProps<typeof Image>["src"];
+  priority: React.ComponentProps<typeof Image>["priority"];
+}
+
+const IconButton = React.forwardRef<HTMLButtonElement, IconButtonProps>(
+  ({ className, iconSrc, priority = true, children, ...props }, ref) => (
+    <Button
+      variant="outline"
+      className={cn("h-full w-full justify-around gap-4 text-wrap", className)}
+      ref={ref}
+      {...props}
+    >
+      <Image alt="" priority={priority} src={iconSrc} />
+      <div className="flex-grow self-start">
+        <div
+          style={{ overflowWrap: "anywhere" }}
+          className="line-clamp-3 text-start"
+        >
+          {children}
+        </div>
       </div>
-    </div>
-  </Button>
-));
+    </Button>
+  ),
+);
 IconButton.displayName = "IconButton";
 
 export { IconButton };
