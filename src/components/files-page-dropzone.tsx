7da657416003048e1d8f1c408ea493150441c2b4
Clément Dreptin <cdreptin@gmail.com>
2025-01-21 10:53:48 +0100

    feat: first basic checks to prevent the upload of directories of multiple files

--- src/components/files-page-dropzone.tsx
+++ src/components/files-page-dropzone.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import * as React from "react";
-import { useDropzone } from "react-dropzone";
+import { useDropzone, type FileWithPath } from "react-dropzone";
+import { displayErrorToast } from "@/lib/utils";
 
 interface FilesPageDropzoneProps {
   children: React.ReactNode;
@@ -10,13 +11,37 @@ interface FilesPageDropzoneProps {
 export default function FilesPageDropzone({
   children,
 }: FilesPageDropzoneProps) {
-  const { getRootProps, getInputProps, isDragActive } = useDropzone();
+  const onDrop = (files: FileWithPath[]) => {
+    if (files.length === 0) {
+      return;
+    }
+
+    const containsDirectory = files.some(
+      (file) => file.path?.indexOf("/") !== file.path?.lastIndexOf("/"),
+    );
+    if (containsDirectory) {
+      displayErrorToast("Uploading a directory isn't supported yet.");
+      return;
+    }
+
+    if (files.length > 1) {
+      displayErrorToast("Uploading multiple files isn't supported yet.");
+      return;
+    }
+
+    console.log(files);
+  };
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    noClick: true,
+  });
 
   return (
     <div {...getRootProps()} className="relative h-full">
       <input {...getInputProps()} />
 
-      {!isDragActive && (
+      {isDragActive && (
         <div className="absolute inset-0 rounded-md bg-gray-600 bg-opacity-50" />
       )}
 
