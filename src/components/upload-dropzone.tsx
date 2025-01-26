af225a7f4b7e75123434d1304c95cd02b62cbb67
Clément Dreptin <cdreptin@gmail.com>
2025-01-26 12:34:15 +0100

    refactor: split the logic into more functions

--- src/components/upload-dropzone.tsx
+++ src/components/upload-dropzone.tsx
@@ -38,7 +38,6 @@ export default function UploadDropzone({ children }: UploadDropzoneProps) {
   const [modalOpen, setModalOpen] = React.useState(false);
   const [confirmModalOpen, setConfirmModalOpen] = React.useState(false);
   const [selectedFiles, setSelectedFiles] = React.useState<FileWithPath[]>([]);
-  const [currentFileName, setCurrentFileName] = React.useState("");
   const [uploadProgress, setUploadProgress] = React.useState(0);
   const [directoriesToCreate, setDirectoriesToCreate] = React.useState<
     string[]
@@ -49,28 +48,31 @@ export default function UploadDropzone({ children }: UploadDropzoneProps) {
   const isError = errorMessage !== "";
   const [, startTransition] = React.useTransition();
 
-  const onDrop = (acceptedFiles: FileWithPath[]) => {
-    if (acceptedFiles.length === 0) {
-      return;
-    }
-
+  const containsDuplicateFiles = (acceptedFiles: FileWithPath[]) => {
     const rootFileNames = getRootFileNames(acceptedFiles);
-    const someFilesAlreadyExist = files.some((file) =>
+
+    return files.some((file) =>
       rootFileNames.some(
         (rootFileName) =>
           rootFileName.toLowerCase() === file.name.toLowerCase(),
       ),
     );
+  };
 
+  const onDrop = (acceptedFiles: FileWithPath[]) => {
     setSelectedFiles(acceptedFiles);
-    if (someFilesAlreadyExist) {
+    if (acceptedFiles.length === 0) {
+      return;
+    }
+
+    if (containsDuplicateFiles(acceptedFiles)) {
       setConfirmModalOpen(true);
     } else {
       void proceedWithUpload(acceptedFiles);
     }
   };
 
-  const confirmUpload: FormAction = async () => {
+  const deleteDuplicateDirectories = async () => {
     const existingDirectories = files.filter((file) => file.isDirectory);
     const rootFileNames = getRootFileNames(selectedFiles);
 
@@ -95,56 +97,74 @@ export default function UploadDropzone({ children }: UploadDropzoneProps) {
       }
     }
 
+    return { success: true };
+  };
+
+  const confirmUpload: FormAction = async () => {
+    const result = await deleteDuplicateDirectories();
+    if (!result.success) {
+      return result;
+    }
+
     setConfirmModalOpen(false);
     void proceedWithUpload(selectedFiles);
 
     return { success: true };
   };
 
-  const proceedWithUpload = async (filesToUpload: FileWithPath[]) => {
-    setModalOpen(true);
-
+  const createDirectoryStructure = async (filesToUpload: FileWithPath[]) => {
     const directories = getDirectories(filesToUpload);
     setDirectoriesToCreate(directories);
 
     const formData = new FormData();
     formData.set("ipAddress", typeof ipAddress === "string" ? ipAddress : "");
 
+    for (let i = 0; i < directories.length; i++) {
+      const directory = directories[i];
+      setDirectoryCreationProgress(i);
+
+      formData.set("parentPath", dirPath);
+      formData.set("dirname", directory);
+
+      const result = await createDirectoryAction(formData);
+      if (result.error != null) {
+        throw result.error;
+      }
+    }
+
+    setDirectoriesToCreate([]);
+  };
+
+  const uploadFiles = async (filesToUpload: FileWithPath[]) => {
+    const formData = new FormData();
+    formData.set("ipAddress", typeof ipAddress === "string" ? ipAddress : "");
+
+    for (let i = 0; i < filesToUpload.length; i++) {
+      const file = filesToUpload[i];
+      setUploadProgress(i);
+
+      const fileDir = pathDirname(file.path ?? "");
+
+      formData.set("dirPath", `${dirPath}\\${fileDir}`);
+      formData.set("file", file);
+
+      const response = await fetch(`${pathname}/upload`, {
+        method: "POST",
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error(await response.text());
+      }
+    }
+  };
+
+  const proceedWithUpload = async (filesToUpload: FileWithPath[]) => {
+    setModalOpen(true);
+
     try {
-      for (let i = 0; i < directories.length; i++) {
-        const directory = directories[i];
-        formData.set("parentPath", dirPath);
-        formData.set("dirname", directory);
-
-        const result = await createDirectoryAction(formData);
-        if (result.error != null) {
-          throw result.error;
-        }
-
-        setDirectoryCreationProgress(i);
-      }
-      setDirectoriesToCreate([]);
-
-      for (let i = 0; i < filesToUpload.length; i++) {
-        const file = filesToUpload[i];
-        setCurrentFileName(file.name);
-
-        const fileDir = pathDirname(file.path ?? "");
-
-        formData.set("dirPath", `${dirPath}\\${fileDir}`);
-        formData.set("file", file);
-
-        const response = await fetch(`${pathname}/upload`, {
-          method: "POST",
-          body: formData,
-        });
-
-        if (!response.ok) {
-          throw new Error(await response.text());
-        }
-
-        setUploadProgress(i);
-      }
+      await createDirectoryStructure(filesToUpload);
+      await uploadFiles(filesToUpload);
 
       setModalOpen(false);
       startTransition(() => {
@@ -176,7 +196,6 @@ export default function UploadDropzone({ children }: UploadDropzoneProps) {
 
       {children}
 
-      {/* Upload progress modal */}
       <Dialog open={modalOpen} onOpenChange={setModalOpen}>
         <DialogContent
           displayCloseButton={isError}
@@ -184,74 +203,22 @@ export default function UploadDropzone({ children }: UploadDropzoneProps) {
           onEscapeKeyDown={!isError ? preventClose : undefined}
           onOpenAutoFocus={preventClose}
         >
-          <DialogHeader>
-            <DialogTitle>Upload progress</DialogTitle>
-            <DialogDescription>
-              {isError ? (
-                <>
-                  Uploading files to <strong>{dirPath}</strong> failed with the
-                  following error.
-                </>
-              ) : directoriesToCreate.length > 0 ? (
-                <>
-                  Creating directories in <strong>{dirPath}</strong>, please
-                  wait...
-                </>
-              ) : (
-                <>
-                  Files are being uploaded to <strong>{dirPath}</strong>, please
-                  wait...
-                </>
-              )}
-            </DialogDescription>
-          </DialogHeader>
-
-          {!isError ? (
-            <div className="flex flex-col gap-2">
-              {directoriesToCreate.length > 0 ? (
-                <>
-                  <p>
-                    Creating directory{" "}
-                    <strong>
-                      {directoriesToCreate[directoryCreationProgress]}
-                    </strong>
-                    ...
-                  </p>
-                  <p>
-                    {directoryCreationProgress.toLocaleString()}&nbsp;/&nbsp;
-                    {directoriesToCreate.length.toLocaleString()}
-                  </p>
-                  <Progress
-                    value={
-                      (directoryCreationProgress / directoriesToCreate.length) *
-                      100
-                    }
-                  />
-                </>
-              ) : (
-                <>
-                  <p>
-                    Uploading <strong>{currentFileName}</strong>...
-                  </p>
-                  <p>
-                    {uploadProgress.toLocaleString()}&nbsp;/&nbsp;
-                    {selectedFiles.length.toLocaleString()}
-                  </p>
-                  <Progress
-                    value={(uploadProgress / selectedFiles.length) * 100}
-                  />
-                </>
-              )}
-            </div>
+          {isError ? (
+            <ErrorModalContent message={errorMessage} />
+          ) : directoriesToCreate.length > 0 ? (
+            <DirectoryCreationModalContent
+              names={directoriesToCreate}
+              progress={directoryCreationProgress}
+            />
           ) : (
-            <p role="alert" className="text-destructive">
-              {errorMessage}
-            </p>
+            <UploadModalContent
+              files={selectedFiles}
+              progress={uploadProgress}
+            />
           )}
         </DialogContent>
       </Dialog>
 
-      {/* Confirm upload modal */}
       <ActionModal
         open={confirmModalOpen}
         onOpenChange={setConfirmModalOpen}
@@ -259,6 +226,98 @@ export default function UploadDropzone({ children }: UploadDropzoneProps) {
         description="Some files already exist. Would you like to replace them?"
       />
     </div>
+  );
+}
+
+interface DirectoryCreationModalContentProps {
+  names: string[];
+  progress: number;
+}
+
+function DirectoryCreationModalContent({
+  names,
+  progress,
+}: DirectoryCreationModalContentProps) {
+  const searchParams = useSearchParams();
+  const dirPath = searchParams.get("path") ?? "";
+
+  return (
+    <>
+      <DialogHeader>
+        <DialogTitle>Upload progress</DialogTitle>
+        <DialogDescription>
+          Creating directories in <strong>{dirPath}</strong>, please wait...
+        </DialogDescription>
+      </DialogHeader>
+
+      <div className="flex flex-col gap-2">
+        <p>
+          Creating directory <strong>{names[progress]}</strong>...
+        </p>
+        <p>
+          {progress.toLocaleString()}&nbsp;/&nbsp;
+          {names.length.toLocaleString()}
+        </p>
+        <Progress value={(progress / names.length) * 100} />
+      </div>
+    </>
+  );
+}
+
+interface UploadModalContentProps {
+  files: FileWithPath[];
+  progress: number;
+}
+
+function UploadModalContent({ files, progress }: UploadModalContentProps) {
+  const searchParams = useSearchParams();
+  const dirPath = searchParams.get("path") ?? "";
+
+  return (
+    <>
+      <DialogHeader>
+        <DialogTitle>Upload progress</DialogTitle>
+        <DialogDescription>
+          Files are being uploaded to <strong>{dirPath}</strong>, please wait...
+        </DialogDescription>
+      </DialogHeader>
+
+      <div className="flex flex-col gap-2">
+        <p>
+          Uploading <strong>{files[progress].name}</strong>...
+        </p>
+        <p>
+          {progress.toLocaleString()}&nbsp;/&nbsp;
+          {files.length.toLocaleString()}
+        </p>
+        <Progress value={(progress / files.length) * 100} />
+      </div>
+    </>
+  );
+}
+
+interface ErrorModalContentProps {
+  message: string;
+}
+
+function ErrorModalContent({ message }: ErrorModalContentProps) {
+  const searchParams = useSearchParams();
+  const dirPath = searchParams.get("path") ?? "";
+
+  return (
+    <>
+      <DialogHeader>
+        <DialogTitle>Upload progress</DialogTitle>
+        <DialogDescription>
+          Uploading files to <strong>{dirPath}</strong> failed with the
+          following error.
+        </DialogDescription>
+      </DialogHeader>
+
+      <p role="alert" className="text-destructive">
+        {message}
+      </p>
+    </>
   );
 }
 
@@ -279,8 +338,17 @@ function pathDirname(path: string) {
 }
 
 function getDirectories(files: FileWithPath[]) {
+  // For a list of files, extract all the directories
+  //
+  // /dir1/dir2/file2.txt      /dir1
+  // /dir1/dir2/file3.txt  =>  /dir1/dir2
+  // /dir1/dir3/file4.txt      /dir1/dir3
+  // /dir1/dir3/file5.txt
+  // /dir1/file.txt
+
   const directories = new Set<string>();
 
+  // Recursively get all the parent directories of each files
   files.forEach((file) => {
     let currentPath = pathDirname(file.path ?? "");
 
@@ -289,6 +357,12 @@ function getDirectories(files: FileWithPath[]) {
       currentPath = pathDirname(currentPath);
     }
   });
+
+  // Sort the directories by depth
+  //
+  // /dir1/dir2           /dir1
+  // /dir1            =>  /dir1/dir2
+  // /dir1/dir2/dir3      /dir1/dir2/dir3
 
   const sortedDirectories = Array.from(directories).sort((a, b) => {
     const depthA = a.split("/").length;
@@ -300,6 +374,13 @@ function getDirectories(files: FileWithPath[]) {
 }
 
 function getRootFileNames(files: FileWithPath[]) {
+  // For a list of files, extract all the file and directory names
+  // at the root
+  //
+  // /dir1/dir2/file3.txt      dir1
+  // /dir1/file2.txt       =>  file1.txt
+  // file1.txt
+
   const rootFileNames = new Set<string>();
 
   for (const file of files) {
