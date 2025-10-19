aaef4ec1c67bc33708722a0bc9224043380dd36d
Clément Dreptin <44998961+ClementDreptin@users.noreply.github.com>
2025-10-19 13:52:36 +0200

    fix(clipboard): clipboard path not always synced with local storage (#25)
    * fix(clipboard): clipboard path not always synced with local storage
    
    * fix: wrong type definition for FilesProviderProps

--- src/contexts/FilesContext.tsx
+++ src/contexts/FilesContext.tsx
@@ -5,16 +5,33 @@ import type { File } from "@/lib/consoles";
 
 interface FilesContextValue {
   files: File[];
+  clipboardPath: string;
+  setClipboardPath: (clipboardPath: string) => void;
 }
 
 const FilesContext = React.createContext<FilesContextValue | null>(null);
 
-interface FilesProviderProps extends FilesContextValue {
+interface FilesProviderProps {
+  files: File[];
   children: React.ReactNode;
 }
 
 export function FilesProvider({ files, children }: FilesProviderProps) {
-  return <FilesContext value={{ files }}>{children}</FilesContext>;
+  const [clipboardPath, setClipboardPath] = React.useState(
+    typeof window !== "undefined"
+      ? (localStorage.getItem("clipboardPath") ?? "")
+      : "",
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem("clipboardPath", clipboardPath);
+  }, [clipboardPath]);
+
+  return (
+    <FilesContext value={{ files, clipboardPath, setClipboardPath }}>
+      {children}
+    </FilesContext>
+  );
 }
 
 export function useFilesContext() {
